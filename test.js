409cc92de9ce01b703ca2c5f575383c784f27285
Jeffrey Leonard <jeffrey9279@gmail.com>
2015-12-11 12:27:48 -0800

    created models

---
+++ test.js
@@ -0,0 +1,7 @@
+var assert = require('assert')
+
+describe('my feature', function(){
+  it('works', function(){
+    assert.equal('A', 'A')
+  })
+})
---
+++ user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose')
+
+module.exports = new mongoose.Schema({
+  profile: {
+    username:{ type: String, required: true, lowercase: true},
+    picture: {type: String, required:true, match: /^http:\/\//i }
+  },
+  data: {
+    oauth: { type: String, required:true },
+    cart: [{
+      product: { type: moongoose.Schema.Types.objectId },
+      quatity: { type: Number, default: 1, min: 1 }
+    }]
+  }
+})
